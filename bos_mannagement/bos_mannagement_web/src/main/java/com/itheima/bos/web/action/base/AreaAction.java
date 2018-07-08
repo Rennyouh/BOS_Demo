f34ef192a3ce9e77afbd0c1090f814345a650117
Rennhy <rennyh@163.com>
2018-07-08 10:31:44 +0800

    完成提取公共类,standardAction和courierAction还未重构; 完成area.html分页查询

--- bos_mannagement/bos_mannagement_web/src/main/java/com/itheima/bos/web/action/base/AreaAction.java
+++ bos_mannagement/bos_mannagement_web/src/main/java/com/itheima/bos/web/action/base/AreaAction.java
@@ -2,6 +2,7 @@ package com.itheima.bos.web.action.base;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,10 +15,14 @@ import org.apache.struts2.convention.annotation.ParentPackage;
 import org.apache.struts2.convention.annotation.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 
 import com.itheima.bos.domain.base.Area;
 import com.itheima.bos.service.AreaService;
+import com.itheima.bos.web.common.BaseAction;
 import com.itheima.utils.PinYin4jUtils;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
@@ -26,82 +31,90 @@ import com.opensymphony.xwork2.ModelDriven;
 @ParentPackage("struts-default")
 @Controller
 @Scope("prototype")
-public class AreaAction extends ActionSupport implements ModelDriven<Area> {
+public class AreaAction extends BaseAction<Area> {
 
 	private static final long serialVersionUID = -4887817007319630888L;
 
-	private Area model = new Area();
-	@Override
-	public Area getModel() {
-		return model;
-	}
-	
 	@Autowired
 	private AreaService areaService;
-	
+
 	private File file;
+
 	public void setFile(File file) {
 		this.file = file;
 	}
 
-	@Action(value="areaAction_importXSL", results= {@Result(name="success", location="/pages/base/area.html", type="redirect")})
+	@Action(value = "areaAction_importXSL", results = {
+			@Result(name = "success", location = "/pages/base/area.html", type = "redirect") })
 	public String importXSL() {
 		try {
 
-            // 用于存放Area的集合
-            // 如果一条一条数据的插入数据库性能太低,所以使用一个集合,一次性进行插入
-            List<Area> list = new ArrayList<>();
+			// 用于存放Area的集合
+			// 如果一条一条数据的插入数据库性能太低,所以使用一个集合,一次性进行插入
+			List<Area> list = new ArrayList<>();
 
-            // 加载文件
-            HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(file));
+			// 加载文件
+			HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(file));
 
-            // 读取第一个工作簿的内容
-            HSSFSheet sheet = workbook.getSheetAt(0);
-            // 遍历所有的行
-            for (Row row : sheet) {
-                // 跳过第一行
-                if (row.getRowNum() == 0) {
-                    continue;
-                }
+			// 读取第一个工作簿的内容
+			HSSFSheet sheet = workbook.getSheetAt(0);
+			// 遍历所有的行
+			for (Row row : sheet) {
+				// 跳过第一行
+				if (row.getRowNum() == 0) {
+					continue;
+				}
 
-                String province = row.getCell(1).getStringCellValue();
-                String city = row.getCell(2).getStringCellValue();
-                String district = row.getCell(3).getStringCellValue();
-                String postcode = row.getCell(4).getStringCellValue();
-                // 截掉省市区的最后一个字符
-                province = province.substring(0, province.length() - 1);
-                city = city.substring(0, city.length() - 1);
-                district = district.substring(0, district.length() - 1);
-                // 生成城市编码
-                String citycode =
-                        PinYin4jUtils.hanziToPinyin(city, "").toUpperCase();
-                // 生成简码
-                String[] headByString = PinYin4jUtils
-                        .getHeadByString(province + city + district);
-                String shortcode =
-                        PinYin4jUtils.stringArrayToString(headByString);
+				String province = row.getCell(1).getStringCellValue();
+				String city = row.getCell(2).getStringCellValue();
+				String district = row.getCell(3).getStringCellValue();
+				String postcode = row.getCell(4).getStringCellValue();
+				// 截掉省市区的最后一个字符
+				province = province.substring(0, province.length() - 1);
+				city = city.substring(0, city.length() - 1);
+				district = district.substring(0, district.length() - 1);
+				// 生成城市编码
+				String citycode = PinYin4jUtils.hanziToPinyin(city, "").toUpperCase();
+				// 生成简码
+				String[] headByString = PinYin4jUtils.getHeadByString(province + city + district);
+				String shortcode = PinYin4jUtils.stringArrayToString(headByString);
 
-                Area area = new Area();
-                area.setProvince(province);
-                area.setCity(city);
-                area.setDistrict(district);
-                area.setPostcode(postcode);
-                area.setCitycode(citycode);
-                area.setShortcode(shortcode);
+				Area area = new Area();
+				area.setProvince(province);
+				area.setCity(city);
+				area.setDistrict(district);
+				area.setPostcode(postcode);
+				area.setCitycode(citycode);
+				area.setShortcode(shortcode);
 
-                list.add(area);
+				list.add(area);
 
-            }
+			}
 
-            areaService.save(list);
-            // 释放资源
-            workbook.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+			areaService.save(list);
+			// 释放资源
+			workbook.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 
-        }
-		
+		}
+
 		return SUCCESS;
+	}
+
+	@Action(value = "areaAction_pageQuery")
+	public String pageQuery() {
+		
+		Pageable pageable = new PageRequest(page - 1, rows);
+		Page<Area> page = areaService.pageQuery(pageable);
+		
+		try {
+			page2Json(page, new String[] {"subareas"});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return NONE;
 	}
 
 }
